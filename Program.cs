03932261875cb6ac30ddb6f71023166716d92342
Jeussu <94780400+Jeussu@users.noreply.github.com>
2023-11-19 10:59:47 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿namespace PolymorphismC
+{
+    internal class Program
+    {
+        // Create a base class Car with two properties HP and Color
+        // Create a Constructor setting those two properties 
+        // Create a Method called ShowDetails() which shows the Hp and Color of the car on console
+        // Create a Repair method which writes "car was repaidred!" onto the console
+        // Create two deriving classes, BMW and Audi, which have their own constructor and have an aditional property
+        // called Model. Also a private member called brand. Brand should be different in each of the two classes.
+        // Create the two methods ShowDetails() and Repair in them as well. Adjust those methods accordingly.
+        static void Main(string[] args)
+        {
+            var cars = new List<Car>
+            {
+                new Audi(200, "blue", "A4"),
+                new BMW(250, "red", "M4")
+            };
+
+            foreach (var car in cars)
+            {
+                car.Repair();
+            }
+
+            Car bmwZ3 = new BMW(200, "black", "Z3");
+            Car audiA3 = new Audi(100, "Green", "A3");
+
+            bmwZ3.ShowDetails();
+            audiA3.ShowDetails();
+
+            BMW bmwM5 = new BMW(330, "While", "M5");
+            bmwM5.ShowDetails();
+
+            Car carB = (Car)bmwM5;
+            carB.ShowDetails();
+
+            Console.ReadKey();
+        }
+    }
+}
